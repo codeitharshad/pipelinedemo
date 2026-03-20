9e4deadd78abfb42505e7b4d066389d6b4d8331b
Harshad Gangurde <143880663+codeitharshad@users.noreply.github.com>
2026-03-20 15:59:54 +0530

    test commit

---
+++ src/test/java/com/example/secondpipelinetest/SecondpipelinetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondpipelinetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondpipelinetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
